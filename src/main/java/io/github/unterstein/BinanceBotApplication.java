03ff69a868b96ba9bbdaaa2a05042b9e94692d0f
alessandro <agmoura@gmail.com>
2018-01-01 21:43:49 -0200

    Enable multiple currency traders

--- src/main/java/io/github/unterstein/BinanceBotApplication.java
+++ src/main/java/io/github/unterstein/BinanceBotApplication.java
@@ -1,5 +1,7 @@
 package io.github.unterstein;
 
+import com.binance.api.client.BinanceApiClientFactory;
+import com.binance.api.client.BinanceApiRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,50 +12,64 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableScheduling
 @SpringBootApplication
 @RestController("/")
 public class BinanceBotApplication {
 
-  private static Logger logger = LoggerFactory.getLogger(BinanceBotApplication.class);
+    private static Logger logger = LoggerFactory.getLogger(BinanceBotApplication.class);
 
-  private BinanceTrader trader;
+    private List<BinanceTrader> traders = new ArrayList<>();
 
-  @Value("${TRADE_DIFFERENCE:0.00000001}")
-  private double tradeDifference;
+    @Value("${TRADE_DIFFERENCE:0.00000001}")
+    private double tradeDifference;
 
-  @Value("${TRADE_PROFIT:1.3}")
-  private double tradeProfit;
+    @Value("${TRADE_PROFIT:1.3}")
+    private double tradeProfit;
 
-  @Value("${TRADE_AMOUNT:150}")
-  private int tradeAmount;
+    @Value("${TRADE_AMOUNT:94}")
+    private int tradeAmount;
 
-  @Value("${BASE_CURRENCY:ETH}")
-  private String baseCurrency;
+    @Value("${BASE_CURRENCY:BTC}")
+    private String baseCurrency;
 
-  @Value("${TRADE_CURRENCY:XVG}")
-  private String tradeCurrency;
+    @Value("${TRADE_CURRENCY:XVG}")
+    private String tradeCurrency;
 
-  @Value("${API_KEY}")
-  private String apiKey;
+    @Value("${API_KEY}")
+    private String apiKey;
 
-  @Value("${API_SECRET}")
-  private String apiSecret;
+    @Value("${API_SECRET}")
+    private String apiSecret;
 
-  @PostConstruct
-  public void init() {
-    logger.info(String.format("Starting app with diff=%.8f, profit=%.8f amount=%d base=%s trade=%s", tradeDifference, tradeProfit, tradeAmount, baseCurrency, tradeCurrency));
-    trader = new BinanceTrader(tradeDifference, tradeProfit, tradeAmount, baseCurrency, tradeCurrency, apiKey, apiSecret);
-  }
+    @PostConstruct
+    public void init() {
 
-  // tick every 3 seconds
-  @Scheduled(fixedRate = 3000)
-  public void schedule() {
-    trader.tick();
-  }
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(apiKey, apiSecret);
+        BinanceApiRestClient client = factory.newRestClient();
 
-  public static void main(String[] args) {
-    SpringApplication.run(BinanceBotApplication.class);
-  }
+        logger.info(String.format("Starting app with diff=%.8f, profit=%.8f amount=%d base=%s trade=%s", tradeDifference, tradeProfit, tradeAmount, baseCurrency, tradeCurrency));
+        traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 94, baseCurrency, "XVG"));
+        //traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 21, baseCurrency, "ADA"));
+        traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 2, baseCurrency, "BNB"));
+        traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 8, baseCurrency, "XRP"));
+        traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 326, baseCurrency, "TRX"));
+        traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 32, baseCurrency, "XLM"));
+        traders.add(new BinanceTrader(client, tradeDifference, tradeProfit, 0.021, baseCurrency, "ETH"));
+    }
+
+    // tick every 3 seconds
+    @Scheduled(fixedRate = 3000)
+    public void schedule() {
+        for (BinanceTrader trader : traders) {
+            trader.tick();
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinanceBotApplication.class);
+    }
 }
--- src/main/java/io/github/unterstein/BinanceTrader.java
+++ src/main/java/io/github/unterstein/BinanceTrader.java
@@ -1,5 +1,6 @@
 package io.github.unterstein;
 
+import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.OrderStatus;
 import com.binance.api.client.domain.account.AssetBalance;
 import com.binance.api.client.domain.account.Order;
@@ -10,123 +11,124 @@ import org.slf4j.LoggerFactory;
 
 public class BinanceTrader {
 
-  private static Logger logger = LoggerFactory.getLogger(BinanceTrader.class);
+    private static Logger logger = LoggerFactory.getLogger(BinanceTrader.class);
 
-  private TradingClient client;
-  private final double tradeDifference;
-  private final double tradeProfit;
-  private final int tradeAmount;
+    private TradingClient client;
+    private final String tradeCurrency;
+    private final double tradeDifference;
+    private final double tradeProfit;
+    private final double tradeAmount;
 
-  private Double currentlyBoughtPrice;
-  private Long orderId;
-  private int panicBuyCounter;
-  private int panicSellCounter;
-  private double trackingLastPrice;
+    private Double currentlyBoughtPrice;
+    private Long orderId;
+    private int panicBuyCounter;
+    private int panicSellCounter;
+    private double trackingLastPrice;
 
-  BinanceTrader(double tradeDifference, double tradeProfit, int tradeAmount, String baseCurrency, String tradeCurrency, String key, String secret) {
-    client = new TradingClient(baseCurrency, tradeCurrency, key, secret);
-    trackingLastPrice = client.lastPrice();
-    this.tradeAmount = tradeAmount;
-    this.tradeProfit = tradeProfit;
-    this.tradeDifference = tradeDifference;
-    clear();
-  }
-
-  void tick() {
-    try {
-      OrderBook orderBook = client.getOrderBook();
-      double lastPrice = client.lastPrice();
-      AssetBalance tradingBalance = client.getTradingBalance();
-      double lastKnownTradingBalance = client.getAllTradingBalance();
-      double lastBid = Double.valueOf(orderBook.getBids().get(0).getPrice());
-      double lastAsk = Double.valueOf(orderBook.getAsks().get(0).getPrice());
-      double buyPrice = lastBid + tradeDifference;
-      double sellPrice = lastAsk - tradeDifference;
-      double profitablePrice = buyPrice + (buyPrice * tradeProfit / 100);
-
-
-      logger.info(String.format("buyPrice:%.8f sellPrice:%.8f bid:%.8f ask:%.8f price:%.8f profit:%.8f diff:%.8f\n", buyPrice, sellPrice, lastAsk, lastAsk, lastPrice, profitablePrice, (lastAsk - profitablePrice)));
-
-      if (orderId == null) {
-        logger.info("nothing bought, let`s check");
-        // find a burst to buy
-        // but make sure price is ascending!
-        if (lastAsk >= profitablePrice) {
-          if (lastPrice > trackingLastPrice) {
-            logger.info("Buy detected");
-            currentlyBoughtPrice = profitablePrice;
-            orderId = client.buy(tradeAmount, buyPrice).getOrderId();
-            panicBuyCounter = 0;
-            panicSellCounter = 0;
-          } else {
-            logger.warn("woooops, price is falling?!? don`t do something!");
-          }
-        } else {
-          logger.info(String.format("No profit detected, difference %.8f\n", lastAsk - profitablePrice));
-          currentlyBoughtPrice = null;
-        }
-        trackingLastPrice = lastPrice;
-      } else {
-        Order order = client.getOrder(orderId);
-        OrderStatus status = order.getStatus();
-        if (status != OrderStatus.CANCELED) {
-          // not new and not canceled, check for profit
-          logger.info("Tradingbalance: " + tradingBalance);
-          if ("0".equals("" + tradingBalance.getLocked().charAt(0)) &&
-              lastAsk >= currentlyBoughtPrice) {
-            if (status == OrderStatus.NEW) {
-              // nothing happened here, maybe cancel as well?
-              panicBuyCounter++;
-              logger.info(String.format("order still new, time %d\n", panicBuyCounter));
-              if (panicBuyCounter > 4) {
-                client.cancelOrder(orderId);
-                clear();
-              }
-            } else {
-              if ("0".equals("" + tradingBalance.getFree().charAt(0))) {
-                logger.warn("no balance in trading money, clearing out");
-                clear();
-              } else if (status == OrderStatus.PARTIALLY_FILLED) {
-                logger.info("partially filled - hodl");
-              } else if (status == OrderStatus.FILLED) {
-                logger.info("Order filled");
-                if (lastAsk >= profitablePrice) {
-                  logger.info("still gaining profitable profits HODL!!");
-                } else {
-                  logger.info("Not gaining enough profit anymore, let`s sell");
-                  logger.info(String.format("Bought %d for %.8f and sell it for %.8f, this is %.8f coins profit", tradeAmount, currentlyBoughtPrice, (1.0 * currentlyBoughtPrice - sellPrice) * tradeAmount));
-                  client.sell(tradeAmount, sellPrice);
-                }
-              } else {
-                // WTF?!
-                logger.error("DETECTED WTF!!!!!");
-                logger.error("Order: " + order);
-                client.panicSell(lastKnownTradingBalance, lastPrice);
-                clear();
-              }
-            }
-          } else {
-            panicSellCounter++;
-            logger.info(String.format("sell request not successful, increasing time %d\n", panicSellCounter));
-            if (panicSellCounter > 3) {
-              client.panicSell(lastKnownTradingBalance, lastPrice);
-              clear();
-            }
-          }
-        } else {
-          logger.warn("Order was canceled, cleaning up.");
-          clear(); // Order was canceled, so clear and go on
-        }
-      }
-    } catch (Exception e) {
-      logger.error("Unable to perform ticker", e);
+    BinanceTrader(BinanceApiRestClient restClient, double tradeDifference, double tradeProfit, double tradeAmount, String baseCurrency, String tradeCurrency) {
+        client = new TradingClient(restClient, baseCurrency, tradeCurrency);
+        trackingLastPrice = client.lastPrice();
+        this.tradeAmount = tradeAmount;
+        this.tradeProfit = tradeProfit;
+        this.tradeDifference = tradeDifference;
+        this.tradeCurrency = tradeCurrency;
+        clear();
     }
-  }
 
-  private void clear() {
-    panicBuyCounter = 0;
-    panicSellCounter = 0;
-    orderId = null;
-    currentlyBoughtPrice = null;
-  }
+    void tick() {
+        try {
+            OrderBook orderBook = client.getOrderBook();
+            double lastPrice = client.lastPrice();
+            AssetBalance tradingBalance = client.getTradingBalance();
+            double lastKnownTradingBalance = client.getAllTradingBalance();
+            double lastBid = Double.valueOf(orderBook.getBids().get(0).getPrice()); // last buy price (bid)
+            double lastAsk = Double.valueOf(orderBook.getAsks().get(0).getPrice()); // last sell price (ask)
+            double buyPrice = lastBid + tradeDifference;
+            double sellPrice = lastAsk - tradeDifference;
+            double profitablePrice = buyPrice + (buyPrice * tradeProfit / 100);
+
+            logger.info(String.format("%s: buyPrice:%.8f sellPrice:%.8f bid:%.8f ask:%.8f price:%.8f profit:%.8f diff:%.8f\n", tradeCurrency, buyPrice, sellPrice, lastAsk, lastAsk, lastPrice, profitablePrice, (lastAsk - profitablePrice)));
+
+            if (orderId == null) {
+                //logger.info("nothing bought, let`s check");
+                // find a burst to buy
+                // but make sure price is ascending!
+                if (lastAsk >= profitablePrice) {
+                    if (lastPrice > trackingLastPrice) {
+                        logger.info("Buy detected");
+                        currentlyBoughtPrice = profitablePrice;
+                        orderId = client.buy(tradeAmount, buyPrice).getOrderId();
+                        panicBuyCounter = 0;
+                        panicSellCounter = 0;
+                    } else {
+                        logger.warn("woooops, price is falling?!? don`t do something!");
+                    }
+                } else {
+                    //logger.info(String.format("No profit detected, difference %.8f\n", lastAsk - profitablePrice));
+                    currentlyBoughtPrice = null;
+                }
+                trackingLastPrice = lastPrice;
+            } else {
+                Order order = client.getOrder(orderId);
+                OrderStatus status = order.getStatus();
+
+                if (status != OrderStatus.CANCELED) {
+                    // not new and not canceled, check for profit
+                    logger.info("Tradingbalance: " + tradingBalance);
+                    if ("0".equals("" + tradingBalance.getLocked().charAt(0)) && lastAsk >= currentlyBoughtPrice) {
+                        if (status == OrderStatus.NEW) {
+                            // nothing happened here, maybe cancel as well?
+                            panicBuyCounter++;
+                            logger.info(String.format("order still new, time %d\n", panicBuyCounter));
+                            if (panicBuyCounter > 4) {
+                                client.cancelOrder(orderId);
+                                clear();
+                            }
+                        } else {
+                            if ("0".equals("" + tradingBalance.getFree().charAt(0))) {
+                                logger.warn("no balance in trading money, clearing out");
+                                clear();
+                            } else if (status == OrderStatus.PARTIALLY_FILLED) {
+                                logger.info("partially filled - hodl");
+                            } else if (status == OrderStatus.FILLED) {
+                                logger.info("Order filled");
+                                if (lastAsk >= profitablePrice) {
+                                    logger.info("still gaining profitable profits HODL!!");
+                                } else {
+                                    logger.info("Not gaining enough profit anymore, let`s sell");
+                                    logger.info(String.format("Bought %d for %.8f and sell it for %.8f, this is %.8f coins profit", tradeAmount, currentlyBoughtPrice, (1.0 * currentlyBoughtPrice - sellPrice) * tradeAmount));
+                                    client.sell(tradeAmount, sellPrice);
+                                }
+                            } else {
+                                // WTF?!
+                                logger.error("DETECTED WTF!!!!!");
+                                logger.error("Order: " + order);
+                                client.panicSell(lastKnownTradingBalance, lastPrice);
+                                clear();
+                            }
+                        }
+                    } else {
+                        panicSellCounter++;
+                        logger.info(String.format("sell request not successful, increasing time %d\n", panicSellCounter));
+                        if (panicSellCounter > 3) {
+                            client.panicSell(lastKnownTradingBalance, lastPrice);
+                            clear();
+                        }
+                    }
+                } else {
+                    logger.warn("Order was canceled, cleaning up.");
+                    clear(); // Order was canceled, so clear and go on
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Unable to perform ticker", e);
+        }
+    }
+
+    private void clear() {
+        panicBuyCounter = 0;
+        panicSellCounter = 0;
+        orderId = null;
+        currentlyBoughtPrice = null;
+    }
 }
